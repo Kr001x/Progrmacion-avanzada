65432abcd148744bf72bc1091f1501fd61744419
Kr001x <A460335459@my.uvm.edu.mx>
2026-08-25 22:39:13 -0600

    Update promedio1.cpp

--- promedio1.cpp
+++ promedio1.cpp
@@ -7,7 +7,7 @@ int main(){
     cout << "Ingresa tu salario mensual ";
     cin >> salario;
     
-if(salario =<8000){
+if(salario <= 8000){
     s_retenido = salario*0.03;
     s_recibido=salario-s_retenido;
 }
